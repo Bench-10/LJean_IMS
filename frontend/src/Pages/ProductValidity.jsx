8e64f0f64e141643c82cf0bb38819bac6754f2d3
Bench-10 <145767536+Bench-10@users.noreply.github.com>
2025-06-26 19:41:00 +0800

    add few designd in product validity page

--- frontend/src/Pages/ProductValidity.jsx
+++ frontend/src/Pages/ProductValidity.jsx
@@ -11,6 +11,45 @@ function ProductValidity() {
 
         <hr className="mt-3 mb-6 border-t-4 border-green-800"/>
 
+        {/*SEARCH AND ADD*/}
+        <div className='flex w-full'>
+          {/*SEARCH */}
+          <div className='w-[400px]'>
+            
+            <input
+              type="text"
+              placeholder="Search Item Name or Category"
+              className="border outline outline-1 outline-gray-400 focus:outline-green-700 focus:py-2 transition-all px-3 py-2 rounded w-full h-9"
+             
+            />
+
+          </div>
+
+          {/*EXPIRY LABEL*/}
+          <div  className="ml-auto flex gap-4 mr-14">
+            
+            {/*NEAR EXPIRY DIV*/}
+            <div className='flex gap-4 align-middle'>
+              <span className="relative pl-6 content-center before:content-[''] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:w-4 before:h-4 before:rounded before:bg-[#FFF3C1]">
+                Near Expiry
+              </span>
+            </div>
+
+
+            {/*EXPIRED DIV*/}
+           <div className='flex gap-4'>
+              <span className="relative pl-6 content-center before:content-[''] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:w-4 before:h-4 before:rounded before:bg-[#FF3131]">
+                Expired
+              </span>
+            </div>
+
+          </div>
+          
+
+        </div>
+
+        <hr className="border-t-2 my-4 w-full border-gray-500"/>
+
 
         
 
