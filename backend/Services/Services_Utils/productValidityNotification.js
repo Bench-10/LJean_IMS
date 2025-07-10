66351e7bb9c5290203c6555bf67bebbcc46d96bc
Bench-10 <145767536+Bench-10@users.noreply.github.com>
2025-07-11 02:00:05 +0800

    finished the notification part whnen it comes to displaying the product expiry

--- backend/Services/Services_Utils/productValidityNotification.js
+++ backend/Services/Services_Utils/productValidityNotification.js
@@ -1,8 +1,10 @@
-import { SQLquery } from "../../db";
+import { SQLquery } from "../../db.js";
 
-export const validityExpiration = async() =>{
+
+//FUNCTION THAT AUTOMATICALLY GENERATE NOTIFICATION FOR PRODUCT SHELF LIFE
+export const notifyProductShelfLife = async() =>{
     const { rows } =  await SQLquery(`
-            SELECT Inventory_product.product_name, quantity_added, (product_validity - CURRENT_DATE) <= 3 AS near_expy, product_validity <= CURRENT_DATE AS expy, Add_stocks.product_id, branch_id, product_validity
+            SELECT Inventory_product.product_name, Category.category_name, (product_validity - CURRENT_DATE) <= 3 AS near_expy, product_validity <= CURRENT_DATE AS expy, Add_stocks.product_id, branch_id, product_validity
             FROM Add_Stocks
             LEFT JOIN Inventory_product USING(product_id)
             LEFT JOIN Category USING(category_id)
@@ -14,13 +16,32 @@ export const validityExpiration = async() =>{
     for (const row of rows){
         //IF THE ROW IS EXPIRED
         if (row.expy){
+
+            //THIS CHECKS THE QUANTITY IF THERE ARE SAME PRODUCT ID WITH THE SAME DATE OF EXPIRY
+            const quantityResult = await SQLquery(
+                `SELECT SUM(quantity_added) AS total_quantity 
+                FROM Add_stocks 
+                WHERE product_validity = $1 AND product_id = $2`,
+                [row.product_validity, row.product_id]
+            );
+
+
+            //NUMBER OF QUANTITY EXPIRED
+            const totalQuantity = quantityResult.rows[0].total_quantity || 0;
+
+
+            //EXPIRED NOTIFICATTION MESSAGE
+            const notificationMessage = `${totalQuantity} of ${row.product_name} has reached the end of shelf life`;
+
+
             // CHECK IF THE PRODUCT EXPIRED IS ALREADY NOTIFIED
             const existing = await SQLquery(
                 `SELECT 1
                 FROM Inventory_alerts 
                 WHERE product_id = $1 
-                AND DATE(alert_date) = $2`,
-                [row.product_id, row.product_validity]
+                AND alert_type = $2
+                AND DATE(alert_date) BETWEEN ($3::date) AND ($3::date + INTERVAL '3 days')`,
+                [row.product_id, 'Expired', row.product_validity]
             );
 
             //IF THERE IS NOTHING EXISTING IN THE NOTIFICATION THEN IT ADDS A NEW NOTIFICATION
@@ -30,19 +51,62 @@ export const validityExpiration = async() =>{
                     (product_id, branch_id, alert_type, message, banner_color)
                     VALUES ($1, $2, $3, $4, $5)
                     RETURNING *`,
-                    [row.product_id, row.branch_id,'Expired',`${row.product_name}`, 'red']
+                    [row.product_id, row.branch_id,'Expired',`${notificationMessage}`, 'red']
                 );
             };
         };
 
+        
+        //IF THE ROW IS NEAR THE END OF ITS SHELF LIFE
+        if (row.near_expy && !row.expy){
 
-        if (row.near_expy){
-            
-        }
+
+            //THIS CHECKS THE QUANTITY IF THERE ARE SAME PRODUCT ID WITH THE SAME DATE OF EXPIRY
+            const quantityResult = await SQLquery(
+                `SELECT SUM(quantity_added) AS total_quantity 
+                FROM Add_stocks 
+                WHERE product_validity = $1 AND product_id = $2`,
+                [row.product_validity, row.product_id]
+            );
+
+
+            //NUMBER OF QUANTITY NEAR EXPIRY
+            const totalQuantity = quantityResult.rows[0].total_quantity || 0;
+
+
+            //EXPIRED NOTIFICATTION MESSAGE
+            const notificationMessage = `${totalQuantity} of ${row.product_name} is reaching the end of shelf life`;
+
+
+            const existing = await SQLquery(
+                `SELECT 1
+                FROM Inventory_alerts 
+                WHERE product_id = $1 
+                AND alert_type = $2
+                AND DATE(alert_date) BETWEEN ($3::date - INTERVAL '3 days') AND ($3::date) `,
+                [row.product_id, 'Near Expired', row.product_validity]
+            );
+
+
+            if (existing.rowCount === 0){
+                await SQLquery(
+                    `INSERT INTO Inventory_Alerts 
+                    (product_id, branch_id, alert_type, message, banner_color)
+                    VALUES ($1, $2, $3, $4, $5)
+                    RETURNING *`,
+                    [row.product_id, row.branch_id, 'Near Expired',`${notificationMessage}`, 'yellow']
+                );
+            };
+        };
     };
-
-    
-
-    return rows;
-
 };
+
+
+
+
+
+
+
+
+
+
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import itemRoutes from './Routes/itemRoutes.js';
+import cron from "node-cron"
+import { notifyProductShelfLife } from './Services/Services_Utils/productValidityNotification.js';
+
 
 const app  = express();
-
 
 dotenv.config();
 
@@ -24,3 +26,9 @@ app.listen(PORT, ()=>{
 app.get("/", (req, res) =>{
     res.send("hello from the backend");
 })
+
+
+//THIS NOTIFIES THE PRODUCT SHELFLIFE EVERY 12 AM
+cron.schedule('57 1 * * *', async () => {
+  notifyProductShelfLife();
+});
