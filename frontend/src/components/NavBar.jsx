b062d10c2803c33f76ec0f60568d6b8184428f41
Bench-10 <145767536+Bench-10@users.noreply.github.com>
2025-07-12 00:06:23 +0800

    enhanches nav bar interactivity

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -16,10 +16,46 @@ function NavBar() {
       <hr className="mt-5 mb-6 border-1 border-white" />
      
       <div className=''>
-        <ul className="flex flex-col gap-2 [&>a]:py-2 [&>a]:px-3 [&>a]:rounded-md [&>a]:transition-all [&>a]:border-l-transparent [&>a]:border-l-4 [&>a]:cursor-pointer [&>a:hover]:bg-[#254717] [&>a:hover]:border-l-emerald-800 [&>a]:flex [&>a]:items-center [&>a]:gap-x-[7px]">
-          <NavLink to="/notification"><IoMdNotifications />Notification</NavLink>
-          <NavLink to="/"><MdOutlineInventory />Inventory</NavLink>
-          <NavLink to="/product_validity"><PiSealWarningBold />Product Validity</NavLink>
+        <ul className="flex flex-col gap-2 [&>a]:py-2 [&>a]:px-3 [&>a]:rounded-md [&>a]:transition-all [&>a]:border-l-4-transparent [&>a]:border-l-green-400 [&>a]:cursor-pointer [&>a:hover]:bg-[#254717] [&>a]:flex [&>a]:items-center [&>a]:gap-x-[7px]">
+
+      
+          <NavLink
+            to="/notification"
+            className={({ isActive }) =>
+              isActive
+                ? "border-l-8 bg-[#254717] border-l-green-400"
+                : ""
+            }
+          >
+            <IoMdNotifications />Notification
+          </NavLink>
+
+
+
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive
+                ? "border-l-8 bg-[#254717] border-l-green-400"
+                : ""
+            }
+          >
+            <MdOutlineInventory />Inventory
+          </NavLink>
+
+
+
+          <NavLink
+            to="/product_validity"
+            className={({ isActive }) =>
+              isActive
+                ? "border-l-8 bg-[#254717] border-l-green-400"
+                : ""
+            }
+          >
+            <PiSealWarningBold />Product Validity
+          </NavLink>
+
         </ul>
       </div>
     </nav>
