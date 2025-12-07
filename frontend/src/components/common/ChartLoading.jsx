92550f1415b379c034071d3c75638c69d4e0340c
Bench-10 <145767536+Bench-10@users.noreply.github.com>
2025-12-07 19:53:44 +0800

    fixed security issue

--- frontend/src/components/common/ChartLoading.jsx
+++ frontend/src/components/common/ChartLoading.jsx
@@ -4,10 +4,10 @@ function ChartLoading({ message = "Loading data...", type='chart', asTableRow = 
   const content = (
     <div className="flex flex-col items-center justify-center h-full w-full bg-white rounded-md py-6">
       {/* BOUNCING THREE DOTS*/}
-      <div className={` flex ${type === 'chart' ? 'space-x-2':'space-x-1'} mt-2`}>
-        <div className={`${type === 'chart' ? 'w-3 h-3':'w-2 h-2'} bg-green-600 rounded-full animate-bounce`} style={{ animationDelay: '0ms' }}></div>
-        <div className={`${type === 'chart' ? 'w-3 h-3':'w-2 h-2'} bg-green-600 rounded-full animate-bounce`} style={{ animationDelay: '150ms' }}></div>
-        <div className={`${type === 'chart' ? 'w-3 h-3':'w-2 h-2'} bg-green-600 rounded-full animate-bounce`} style={{ animationDelay: '300ms' }}></div>
+      <div className={`flex ${type === 'chart' ? 'space-x-2' : 'space-x-1'} mt-2`}>
+        <div className={`${type === 'chart' ? 'w-3 h-3' : 'w-2 h-2'} bg-green-600 rounded-full animate-bounce animation-delay-0`}></div>
+        <div className={`${type === 'chart' ? 'w-3 h-3' : 'w-2 h-2'} bg-green-600 rounded-full animate-bounce animation-delay-150`}></div>
+        <div className={`${type === 'chart' ? 'w-3 h-3' : 'w-2 h-2'} bg-green-600 rounded-full animate-bounce animation-delay-300`}></div>
       </div>
 
       {/* LOADING TEXT */}
